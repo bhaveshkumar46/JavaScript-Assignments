fda5d96d49ac1763c3b6d180c4ce371703427142
bhaveshkumar46 <bhaweshking46@gmail.com>
2025-01-04 16:45:49 +0500

    folder

---
+++ IF ELSE/app.js
@@ -0,0 +1,223 @@
+// Q# 01
+// var num = prompt ("ENTER A NUMBER") ;
+// if(num % 3===0 ){
+//     console.log("Number is divisible by 3");
+// } else {
+//     console.log("Number is not divisible by 3");
+// }
+
+
+
+
+// Q# 02:
+// var num = +prompt("ENTER A NUMBER");
+// if(num % 2===0 ){
+//     console.log("ITS AN EVEN NUMBER");
+// } else{
+//     console.log("ITS AN ODD NUMBER");
+// }
+
+
+// Q# 03:
+// var num = +prompt("ENTER A NUMBER");
+// if(num >= 18 ){
+//     console.log("Old enough");
+// } else{
+//     console.log("Too young");
+// }
+
+
+// Q# 04:
+// var username = +prompt("ENTER A NUMBER");
+// var myname = "bhavesh";
+// if (username === myname) {
+//     console.log("Hello,! Nice to meet you!");
+// }
+
+
+// Q# 05:
+// var num = +prompt("ENTER A NUMBER") ;
+// if(num % 3===0 ){
+//     console.log("Number is divisible by 3");
+// } else {
+//     console.log("Number is not divisible by 3");
+// }
+
+
+// Q# 06:
+// this is done by using AI-CHATBOX
+// // Prompt the user to enter a character (number or string)
+// var input = prompt("Enter a character (number or string):");
+// // Check if the input is a number, uppercase letter, or lowercase letter
+// if (!isNaN(input)) {
+//     console.log("The input is a number.");
+// } else {
+//     var charCode = input.charCodeAt(0);
+//     if (charCode >= 65 && charCode <= 90) {
+//         console.log("The input is an uppercase letter.");
+//     } else if (charCode >= 97 && charCode <= 122) {
+//         console.log("The input is a lowercase letter.");
+//     } else {
+//         console.log("The input is neither a number nor a letter.");
+//     }
+// }
+
+
+
+
+    // Q# 07:
+//     var num1 = +prompt("ENTER NUM 1") 
+//     var num2 = +prompt("ENTER NUM 2")
+//     var operator = prompt("ENTER operator")
+
+//     if (operator == "+") {
+//         console.log("HERE IS THE VALUE" , num1 + num2);    
+//     }
+//    else if (operator == "-") {
+//     console.log("HERE IS THE VALUE", num1 - num2);
+//    }
+//    else if (operator == "*") {
+//     console.log("HERE IS THE VALUE", num1 * num2);
+//    }
+//    else if (operator == "/") {
+//     console.log("HERE IS THE VALUE", num1 / num2);
+//    }
+//    else {
+//     console.log("NO DATA");
+//    }
+
+
+
+
+    // Q# 08:
+// var time = +prompt("Enter time in 24-hour format")
+// if (time >= 0 && time< 12) {
+//     console.log("GOOD MORNING");
+// }
+// else if (time >= 12 && time< 17) {
+//     console.log("GOOD AFTER-NOON");
+// }
+// else if (time >= 17 && time< 21) {
+//     console.log("GOOD EVENING");
+// }
+// else if (time >= 21 && time< 24) {
+//     console.log("GOOD NIGHT");
+// }
+// else{
+//     console.log("INVALID TIME");
+    
+// }
+
+
+// Q# 09:
+// var year = +prompt("ENTER A YEAR")
+// if (year % 4 == 0 ) {
+//     console.log("ITS A LEAP YEAR");
+// }
+// else{
+//     console.log("ITS NOT A LEAP YEAR");
+    
+// }
+
+
+// Q# 10:
+// var password = "0345";
+// var pass = prompt("Please enter your PIN");
+// if (pass === password) {
+//     console.log("Correct! The password you entered matches the original password.");
+// } else {
+//     console.log("Incorrect password");
+// }
+
+
+
+// Q# 11:
+// var firstname = "Ali";
+// if (firstname == "Fahad") {
+//     console.log("Hello Fahad");
+// }
+// else{
+//     console.log("You are not Fahad");
+// }
+
+
+// Q# 12:
+// var greeting;
+// var hour = 13;
+// if (hour<18) {
+//     console.log("Good day");
+// }
+// else{
+//     console.log("Good evening");
+    
+// }
+
+
+// Q# 13:
+// var num1 = +prompt("Enter the first integer:")
+// var num2 = +prompt("Enter the second integer:")
+
+// if (num1 > num2) {
+//     console.log("The larger number is: " + num1);
+// }
+// else if (num2 > num1) {
+//     console.log("The larger number is: " + num2);
+
+// }
+// else{
+//     console.log("Both numbers are equal");
+// }
+
+
+
+
+// Q# 14:
+// var num = +prompt("Enter a number")
+// if (num > 0 ) {
+//     console.log("Number is positive");
+// }
+// else if (num < 0) {
+//     console.log("Number is negative");
+// }
+
+
+// Q# 15:
+// var time = +prompt("Enter time in 24-hour format")
+// if (time >= 6 && time< 9) {
+//     console.log("Breakfast is served");
+// }
+// else if (time >= 11 && time< 13) {
+//     console.log("Time for lunch");
+// }
+// else if (time >= 17 && time< 20) {
+//     console.log("It's dinner time");
+// }
+// else{
+//     console.log("Sorry, you'll have to wait, or go get a snack.");
+    
+// }
+
+
+// Q# 16:
+// var input = prompt("Enter a variable")
+// if (input == "number") {
+//     console.log("The type of value is: number");
+// }
+// else if (input == "string") {
+//     console.log("The type of value is: string");
+// }
+// else if (input == "Boolean") {
+//     console.log("The type of value is: boolean");
+// }
+// else if (input == "undefined") {
+//     console.log("The type of value is: undefined");
+// }
+// else{
+//     console.log("The type of value is: unkown");
+    
+// }
+
+
+
+
+// Q# 17:
